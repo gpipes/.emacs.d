6b9f5282b3755c3f97f9f13f38c7f57af0f9734b
Gerry Pipes <GPipes@carbonite.com>
2018-01-26 10:50:30 -0500

    add debugging capability, projectile, theme and more modes

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (package-initialize)
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(setq debug-file (expand-file-name "gud.el" user-emacs-directory))
 (load custom-file)
-
+(load debug-file)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                Setup Packages               ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -15,7 +16,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;               Require Packages              ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(use-package cl)
+(use-package ag)
 (use-package ivy
   :init (ivy-mode 1)
   :bind (("M-x" . counsel-M-x)
@@ -24,7 +26,36 @@
 
 (use-package magit)
 
-(use-package cmake-ide)
+(use-package projectile
+  :init (projectile-mode))
+
+(use-package clang-format)
+
+(use-package exec-path-from-shell
+  :init (exec-path-from-shell-initialize))
+
+(use-package rtags)
+(use-package cmake-ide
+  :init
+  (require 'rtags)
+  (cmake-ide-setup)
+  :bind
+  (("C-." . rtags-find-symbol-at-point)
+   ("C-," . rtags-find-references-at-point)
+   ("M-<left>" . rtags-location-stack-back)
+   ("M-<right>" . rtags-location-stack-forward)))
 
 (use-package which-key)
 
+(use-package cyberpunk-theme)
+(load-theme 'cyberpunk t)
+
+(use-package cc-mode
+  :bind
+  (:map c++-mode-map
+	("M-a" . c-beginning-of-defun)
+	("C-M-a" . c-beginning-of-statement)))
+
+(use-package markdown-mode)
+(use-package flymd)
+(use-package cmake-mode)
