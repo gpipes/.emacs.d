93ea9862b4205f816cf1845e6dfa42cde8527d7a
GPipes <gpipes@carbonite.com>
2020-10-06 21:47:36 -0400

    inconsolata looks like... not good at all on windows

--- custom.el
+++ custom.el
@@ -56,4 +56,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend t :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-bold :height 180 :width normal :foundry "nil" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :extend t :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 143 :width normal :foundry "outline" :family "Ubuntu Mono")))))
