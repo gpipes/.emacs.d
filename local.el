e6de5a0064a05d62910be3d664eb486fecefb86e
GPipes <gpipes@carbonite.com>
2020-10-20 19:23:18 -0400

    a write up because I will inevitable forget the workings of emacs

--- local.el
+++ local.el
@@ -1,7 +1,18 @@
 ;;; local.el -- Gerry Pipes' local load file list
+
 ;;; Commentary:
+;; define a list of files on this local machine that should be loaded
+;; on startup.  These may be in different spaces per machine so let
+;; this be defined whenever initialize the variable for use (if a
+;; parent does this this is ignored) then fill it with whatever.  setq
+;; is required so parents can set their own defaults if this file is
+;; absent
+
 ;;; Code:
+
 (defvar local-el-files-list '())
-(setq local-el-files-list '())
+(setq local-el-files-list
+      '("~/devcenter/MacClient/Toolbox/CarboniteMac.el"))
 (provide 'local)
+
 ;;; local.el ends here
