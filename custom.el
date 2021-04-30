a23d6cbc6e9684622a9d34364d7294015a73e4db
GPipes <gpipes@carbonite.com>
2021-04-30 17:27:02 -0400

    whoops too many parens deleted

--- custom.el
+++ custom.el
@@ -37,7 +37,7 @@
  '(truncate-lines t)
  '(use-package-always-ensure t)
  '(which-function-mode t)
- '(which-key-mode t)
+ '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
