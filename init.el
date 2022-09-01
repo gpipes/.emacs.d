09509e27a5d006cf266828dcb2ff6d148c7a90a9
GPipes <gpipes@carbonite.com>
2022-09-01 11:17:48 -0400

    simplify init and custom a little more

--- init.el
+++ init.el
@@ -6,12 +6,6 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 (load-theme 'wheatgrass)
-(add-hook 'dired-load-hook
-          (lambda () (load "dired-x")))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                Setup Packages               ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -21,9 +15,6 @@
 
 (require 'use-package)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;               Require Packages              ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package clang-format)
 (use-package astyle)
 (use-package which-key)
@@ -39,16 +30,15 @@
 (use-package markdown-mode)
 (use-package cmake-mode)
 
+(use-package org
+  :hook
+  (org-mode . turn-on-auto-fill)
+  (text-mode . turn-on-auto-fill))
+
 (use-package geiser)
 (use-package geiser-guile)
 (use-package geiser-racket)
 (use-package pytest)
-
-(use-package org
-  :bind ("C-c a" . org-agenda)
-  :hook ;; turn on auto fill for text/organization modes
-  (org-mode . turn-on-auto-fill)
-  (text-mode . turn-on-auto-fill))
 
 (use-package ws-butler
   :init (ws-butler-global-mode))
@@ -56,14 +46,12 @@
 (add-to-list 'auto-mode-alist '("\\.sj\\'" . javascript-mode))
 (add-to-list 'auto-mode-alist '("Jenkinsfile\\'" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.m\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.mm\\'" . c++-mode))
 (add-to-list 'semantic-symref-filepattern-alist
              '(dired-mode "*.*"))
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(put 'scroll-left 'disabled nil)
 (show-paren-mode)
 
 (provide 'init)
 ;;; init.el ends here
-(put 'scroll-left 'disabled nil)
