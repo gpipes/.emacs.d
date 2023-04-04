fe4537b781ee62461ab2107390f8fed727826882
GPipes <gpipes@carbonite.com>
2023-04-04 11:04:08 -0400

    view read only and some cleanup

--- init.el
+++ init.el
@@ -2,12 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(epg-pinentry-mode 'loopback)
- '(file-mode-list
-   '(("\\.sj\\'" . javascript-mode)
-     ("Jenkinsfile\\'" . groovy-mode)
-     ("\\.h\\'" . c++-mode)))
- '(fill-column 80)
+ '(file-mode-list '(("Jenkinsfile\\'" . groovy-mode)))
+ '(fill-column 75)
  '(global-display-fill-column-indicator-mode t)
  '(global-font-lock-mode nil)
  '(indent-tabs-mode nil)
@@ -19,13 +20,14 @@
  '(package-archives
    '(("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "http://melpa.org/packages/")))
- '(package-selected-packages
-   '(password-store powershell groovy-mode csharp-mode))
+ '(package-selected-packages '(password-store powershell groovy-mode
+                                              csharp-mode))
  '(password-store-password-length 10)
  '(scheme-program-name "csi")
  '(tool-bar-mode 'nil)
  '(tramp-remote-path '(tramp-own-remote-path))
- '(tramp-sh-extra-args nil))
+ '(tramp-sh-extra-args nil)
+ '(view-read-only t))
 
 (package-install-selected-packages)
 (mapc 'require package-selected-packages)
@@ -36,9 +38,14 @@
 (mapc 'add-to-auto-mode file-mode-list)
 
 (put 'narrow-to-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
 (set-face-attribute 'default nil :height 180)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (provide 'init)
 ;;; init.el ends here
-
