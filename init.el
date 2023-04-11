226d82fcd772b30ee4a634b514c2885a28e0f333
GPipes <gpipes@carbonite.com>
2023-04-11 11:52:23 -0400

    set font in faces and use window size not fill column

--- init.el
+++ init.el
@@ -6,15 +6,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(allow-func-list '(narrow-to-region))
  '(epg-pinentry-mode 'loopback)
  '(file-mode-list '(("Jenkinsfile\\'" . groovy-mode)))
- '(fill-column 75)
- '(global-display-fill-column-indicator-mode t)
  '(global-font-lock-mode nil)
  '(indent-tabs-mode nil)
  '(menu-bar-mode 'nil)
  '(org-agenda-files '("~/notes/log.org"))
- '(org-clock-mode-line-total 'current)
  '(org-log-done 'note)
  '(org-log-note-clock-out t)
  '(package-archives
@@ -23,7 +21,6 @@
  '(package-selected-packages '(password-store powershell groovy-mode
                                               csharp-mode))
  '(password-store-password-length 10)
- '(scheme-program-name "csi")
  '(tool-bar-mode 'nil)
  '(tramp-remote-path '(tramp-own-remote-path))
  '(tramp-sh-extra-args nil)
@@ -37,15 +34,17 @@
   (add-to-list 'auto-mode-alist element))
 (mapc 'add-to-auto-mode file-mode-list)
 
-(put 'narrow-to-region 'disabled nil)
-(set-face-attribute 'default nil :height 180)
+(defcustom allow-func-list nil "List of allowed disabled functions")
+(defun allow-func (func-sym)
+  (put func-sym 'disabled nil))
+(mapc 'allow-func allow-func-list)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:height 160)))))
 
 (provide 'init)
 ;;; init.el ends here
