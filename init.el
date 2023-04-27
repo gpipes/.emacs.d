8476b75a3b2f2822a70bd9d46d3f9f8c3cfdebb2
gerry <gerrypipes@hotmail.com>
2023-04-27 01:07:43 -0400

    use ubuntu mono and cdb-gud isn't working well enough

--- init.el
+++ init.el
@@ -39,7 +39,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 240)))))
+ '(default ((t (:height 181 :family "Ubuntu Mono" :foundry "outline"
+						:slant normal :weight normal :width normal)))))
 
 (provide 'init)
 ;;; init.el ends here
