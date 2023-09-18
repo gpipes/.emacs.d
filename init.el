a5622d4d594899dbb3407a5486a67356d03a4129
gpipes <gpipes@carbonite.com>
2023-09-18 11:03:09 -0400

    ignore case in minibuf + no more ido mode

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
  '(org-log-done 'note)
  '(org-log-note-clock-out t)
  '(python-shell-interpreter "python")
+ '(read-buffer-completion-ignore-case t)
  '(tab-width 4)
  '(tramp-remote-path '(tramp-own-remote-path))
  '(tramp-sh-extra-args nil)
@@ -36,14 +37,12 @@
   (put func-sym 'disabled nil))
 (mapc 'allow-func allow-func-list)
 
-(ido-mode t)
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180 :family "Ubuntu Mono")))))
+ )
 
 (provide 'init)
 ;;; init.el ends here
